cfabcfffc7623013d561b1454e01d6c281a92b1b
Naza1re <mcarim@mail.ru>
2023-09-13 23:05:01 +0300

    Initial commit

---
+++ src/main/java/com/RESTforANDROID/RESTforAndroid/ResTforAndroidApplication.java
@@ -0,0 +1,13 @@
+package com.RESTforANDROID.RESTforAndroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTforAndroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTforAndroidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RESTforANDROID/RESTforAndroid/ResTforAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESTforANDROID.RESTforAndroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTforAndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
